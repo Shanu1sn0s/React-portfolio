216b182345c03b214a67e5661419d421dfd911a6
Olayinka Shanu <ademayowa4@gmail.com>
2023-07-28 15:58:42 +0800

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a172f] flex justify-center items-center p-4 text-gray-300"
+    >
+      <form action="https://getform.io/f/a29a6be5-577c-44f0-8024-730d8abf6918" method="POST" className="flex flex-col max-w-[600px] w-full text-gray-900">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-red-600">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            / / Submit the form or send me an email - Shanuolayinka4@gmail.com
+          </p>
+        </div>
+        <input
+          className="bg-[#ccd6f6] p-2 "
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6] "
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea className="bg-[#ccd6f6] text-gray-800 p-2" placeholder="Message" name="message" rows="10"></textarea>
+        <button className="text-white border-2 hover:bg-red-600 hover:border-red-600 px-4 py-3 my-8 mx-auto flex items-center">
+            Let's Collaborate!
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a172f]">
+      {/*Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-red-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Shanu Olayinka
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Frontend Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am a frontend developer who crafts monumental experiences by
+          creating responsive websites and web applications. Leveraging
+          React.js, JavaScript, CSS, and HTML, I push the boundaries of
+          innovation. With Git and Node.js, I ensure efficient collaboration and
+          scalability, showcasing my expertise in shaping the future of web
+          development.
+        </p>
+        <div>
+          <button className="group text-white border-2 px-3 py-6 my-2 flex item-center hover:bg-red-600 hover:border-red-600 ">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+ 
